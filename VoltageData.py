0b5c3d43823d608a5827db93149a89348ffcd32c
PietroCignoni <pietro.cignoni@gmail.com>
2024-10-08 10:10:05 +0200

    Updates

--- VoltageData.py
+++ VoltageData.py
@@ -1,8 +1,18 @@
-import logging
-logger = logging.getLogger(__name__)
+from dataclasses import dataclass
+from venv import logger
 
 
+@dataclass
 class Reading:
+
+    timestamp: float
+    adc: int
+    def voltage(self):
+        return 1.653 * self.adc + 0.456
+
+
+
+class OldReading:
     """Storage for a single voltage"""
     def __init__(self, timestamp, adc):
         self.timestamp = timestamp
@@ -22,12 +32,13 @@ class VoltageData:
     def __init__(self, file_path):
         logger.info(f'Reading voltage data from (file_path)...')
         with open(file_path) as input_file:
-            self._lines = input_file.readlines()
-        logger.info(f'Done, {len(self._lines)} values read.')
-        self._line_iterator = iter(self._lines)
+            #self._lines = input_file.readlines()
+            self._readings = [self.parse_line(line) for line in input_file.readlines()]
+        logger.info(f'Done, {len(self._readings)} values read.')
+        self._iterator = iter(self._readings)
 
 
-    def parse_Line(self, line):
+    def parse_line(self, line):
         """Parse a single line from a txt file and returns a Reading object"""
         timestamp, adc = line.split()
         timestamp = float(timestamp)
@@ -40,9 +51,17 @@ class VoltageData:
     
 
     def __next__(self):
-        return self.parse_Line(next(self._line_iterator))
+        #return self.parse_Line(next(self._line_iterator))
+        return next(self._iterator)
+    
+    def __getitem__(self, index):
+        return self._readings[index]
 
 
 if __name__ == '__main__':
-    for reading in VoltageData('voltage_data.txt'):
+    data = VoltageData('voltage_data.txt')
+    for reading in data:
         print(reading)
+
+    print('Done')
+    print(data[3])
